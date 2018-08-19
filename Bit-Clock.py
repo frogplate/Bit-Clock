25dc82c0f119d0df91c3606e2efde8bca6582279
Jonathan Littlewood <frogplate@gmail.com>
2018-08-19 14:03:11 +0100

    Updated variable naming to match PEP 8

--- Bit-Clock.py
+++ Bit-Clock.py
@@ -1,26 +1,26 @@
 from microbit import *
 
+BIT_BRIGHT = 9
+
 display.clear()
 start_time = running_time()
 base_hrs = 23
 base_mins = 44
 last = 0
 
-def binary(col, value):
+def display_binary(col, value):
+    
     if value in [1, 3, 5, 7, 9]:
-        display.set_pixel(col, 3, 9)
+        display.set_pixel(col, 3, BIT_BRIGHT)
         
     if value in [2, 3, 6, 7]:
-        display.set_pixel(col, 2, 9)
+        display.set_pixel(col, 2, BIT_BRIGHT)
 
     if value in [4, 5, 6, 7]:
-        display.set_pixel(col, 1, 9)
-        
-    if value in [6, 7]:
-        display.set_pixel(col, 2, 9)
+        display.set_pixel(col, 1, BIT_BRIGHT)
         
     if value in [8, 9]:
-        display.set_pixel(col, 0, 9)
+        display.set_pixel(col, 0, BIT_BRIGHT)
 
 while True:
     run_secs = int((running_time() - start_time) / 1000)
@@ -30,15 +30,15 @@ while True:
     run_mins = int((run_hrs - int(run_hrs)) * 60)
     run_hrs = int(run_hrs) % 24
   
-    col1 = int(run_hrs / 10)
-    col2 = run_hrs - 10 * col1
-    col4 = int(run_mins / 10)
-    col5 = run_mins - 10 * col4
+    hrs_tens_value = int(run_hrs / 10)
+    hrs_unit_value = run_hrs - 10 * hrs_tens_value
+    min_tens_value = int(run_mins / 10)
+    min_unit_value = run_mins - 10 * min_tens_value
 
-    if last != col5:  
+    if last != min_unit_value:  
         display.clear()
-        binary(0, col1)
-        binary(1, col2)
-        binary(3, col4)
-        binary(4, col5)
-        last = col5
+        display_binary(0, hrs_tens_value)
+        display_binary(1, hrs_unit_value)
+        display_binary(3, min_tens_value)
+        display_binary(4, min_unit_value)
+        last = min_unit_value
