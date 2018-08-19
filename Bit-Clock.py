f2884a653516be7f42cec6d611504dfc1ad1c6c0
Jonathan Littlewood <frogplate@gmail.com>
2018-08-19 14:22:19 +0100

    Button A increments hours, button B increments minutes

--- Bit-Clock.py
+++ Bit-Clock.py
@@ -4,9 +4,9 @@ BIT_BRIGHT = 9
 
 display.clear()
 start_time = running_time()
-base_hrs = 23
-base_mins = 44
-last = 0
+base_hrs = 12
+base_mins = 34
+last = -1
 
 def display_binary(col, value):
     
@@ -22,7 +22,29 @@ def display_binary(col, value):
     if value in [8, 9]:
         display.set_pixel(col, 0, BIT_BRIGHT)
 
+def refresh_display(hrs_tens, hrs_units, min_tens, min_units):
+    display.clear()
+    display_binary(0, hrs_tens)
+    display_binary(1, hrs_units)
+    display_binary(3, min_tens)
+    display_binary(4, min_units)
+    
 while True:
+    
+    # Button A increments the hours
+    if button_a.was_pressed():
+        base_hrs = base_hrs + 1
+        if base_hrs > 23:
+            base_hrs = 0
+        last = -1
+
+    # Button B increments the minutes
+    if button_b.was_pressed():
+        base_mins = base_mins + 1
+        if base_mins > 59:
+            base_mins = 0
+        last = -1
+        
     run_secs = int((running_time() - start_time) / 1000)
 
     run_hrs = run_secs / 3600 + base_hrs + base_mins / 60
@@ -30,15 +52,11 @@ while True:
     run_mins = int((run_hrs - int(run_hrs)) * 60)
     run_hrs = int(run_hrs) % 24
   
-    hrs_tens_value = int(run_hrs / 10)
-    hrs_unit_value = run_hrs - 10 * hrs_tens_value
-    min_tens_value = int(run_mins / 10)
-    min_unit_value = run_mins - 10 * min_tens_value
+    hrs_tens  = int(run_hrs / 10)
+    hrs_units = run_hrs - 10 * hrs_tens
+    min_tens  = int(run_mins / 10)
+    min_units = run_mins - 10 * min_tens
 
-    if last != min_unit_value:  
-        display.clear()
-        display_binary(0, hrs_tens_value)
-        display_binary(1, hrs_unit_value)
-        display_binary(3, min_tens_value)
-        display_binary(4, min_unit_value)
-        last = min_unit_value
+    if last != min_units:  
+        refresh_display(hrs_tens, hrs_units, min_tens, min_units)
+        last = min_units
